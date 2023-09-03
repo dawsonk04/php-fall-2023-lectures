135c1ab6c0d5cc4c8eeea78afcf37dc36c607fb3
Dawson Knudtson <knudtdaw0000@gmail.com>
2023-09-03 16:49:03 -0500

    template for future class projects - first assingment completed

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dawson's Webpage</title>
+    <link type="text/css" rel="stylesheet" href="css/base.css">
+</head>
+<body>
+    <header>
+        <h1>2023 Fall - Php Class (Dawson's Website) </h1>
+    </header>
+
+    <nav>
+       <ul>
+           <li><a href="/">Homepage</a></li>
+           <li><a href="#">Loop</a> </li>
+           <li><a href="#">Countdown</a> </li>
+           <li><a href="#">Other</a> </li>
+
+       </ul>
+    </nav>
+
+    <main>
+        <img src="image/apple.png" alt="picture of apple logo" >
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem facilis quae quos repellendus tempore, veniam. Aliquid amet aperiam aspernatur delectus distinctio earum eos id inventore iure maxime, nesciunt nulla numquam omnis provident quam, quisquam, rem sint ut vitae voluptatum? Animi aperiam eligendi et minima minus nemo, optio perspiciatis porro praesentium quaerat quam quo reiciendis rem repellendus sint? Aliquid architecto atque beatae dolore fugiat impedit, minus optio quam quibusdam quo quos sint, sit unde, vero voluptatem! Ab deleniti, deserunt iure molestias nam officia similique sint veniam voluptas voluptatibus! Aperiam beatae consectetur distinctio ducimus error necessitatibus odit officia perspiciatis sint, temporibus vero.</p>
+    </main>
+    <footer>
+
+        <p> &copy; 2023 Dawson.com </p> </footer>
+</body>
+</html>
