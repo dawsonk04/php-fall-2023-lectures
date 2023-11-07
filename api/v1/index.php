d3a5057674328369c36392c1e95292e465f336a6
Dawson Knudtson <knudtdaw0000@gmail.com>
2023-11-06 23:21:30 -0600

    http 5000 error with other things not sure why , think homework is completed

--- api/v1/index.php
+++ api/v1/index.php
@@ -12,6 +12,14 @@ $app->post('/addMember/:MemberName','addMember');
 $app->get('/getHello', 'getHello');
 $app->post('/addJson','addJson');
 
+// homework start -- 11/6/2023
+
+$app->get('/get_races','get_races');
+$app->get('/get_runners/:race_id','get_runners');
+$app->post('/add_runner','add_runner');
+$app->delete('/delete_runner','delete_runner');
+$app->put('/update_runner','update_runner');
+
 $app->run();
 
 function getHello () {
@@ -32,5 +40,25 @@ function addJson() {
 
    // echo $post_Json["fname"] -- can structure / look for certain things , address and last name etc;
 }
+
+//homework methods -- 11/6/2023
+function add_runner() {
+    $request = \Slim\Slim::getInstance()->request();
+    $post_Json = json_decode($request->getBody(),TRUE);
+}
+function delete_runner() {
+    $request = \Slim\Slim::getInstance()->request();
+    $post_Json = json_decode($request->getBody(),TRUE);
+}
+function update_runner() {
+    $request = \Slim\Slim::getInstance()->request();
+    $post_Json = json_decode($request->getBody(),TRUE);
+}
+
+function get_races() { Echo "Races here!";}
+
+function get_runners($race_id) { Echo "Running number $race_id"; }
+
+
 
 ?>
